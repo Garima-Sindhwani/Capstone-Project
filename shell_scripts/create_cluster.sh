fdbdc8e27dfe2889f2f4285c0744eb276403fae8
Garima Sindhwani <gsindhwani23@gmail.com>
2023-05-07 23:32:04 +0530

    deploy blue

--- shell_scripts/create_cluster.sh
+++ shell_scripts/create_cluster.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-CLUSTER_NAME=capstone-project
-REGION=us-east-1
+cluster_name=capstone-project
+aws_region=us-east-1
 
-if /.eksctl utils describe-stacks --region=${REGION} --cluster=${CLUSTER_NAME} | grep -q ${CLUSTER_NAME}; then
+if /.eksctl utils describe-stacks --region=${aws_region} --cluster=${cluster_name} | grep -q ${CLUSTER_NAME}; then
     echo "Cluster already exists!"
 else
     echo "Creating Cluster..."
-    /.eksctl create cluster --name ${CLUSTER_NAME} --region=${REGION}
+    /.eksctl create cluster --name ${cluster_name} --region=${aws_region}
     echo "Cluster Created."
 fi
---
+++ shell_scripts/deploy_app_blue.sh
@@ -0,0 +1,10 @@
+# Get Nodes details
+/.kubectl get nodes
+
+# Create the deployment and loadbalancer
+/.kubectl apply -f manifests/deploy_blue.yml
+
+# Get details for loadbalancer
+/.kubectl get service/loadbalancer-service
+# Get details for deployment
+/.kubectl get deploy,rs,svc,pods
