6336a4c2e3756b6047081067b418545e4348a8a6
Garima Sindhwani <gsindhwani23@gmail.com>
2023-05-07 22:28:13 +0530

    correct cluster name

--- shell_scripts/create_cluster.sh
+++ shell_scripts/create_cluster.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CLUSTER_NAME=capstone_cluster
+CLUSTER_NAME=capstone-cluster
 REGION=us-east-1
 
 if /.eksctl utils describe-stacks --region=${REGION} --cluster=${CLUSTER_NAME} | grep -q ${CLUSTER_NAME}; then
