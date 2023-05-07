5ea084a100b41915bde6bda15cf838c817f42d9d
Garima Sindhwani <gsindhwani23@gmail.com>
2023-05-07 22:26:12 +0530

    eksctl command not found

--- shell_scripts/create_cluster.sh
+++ shell_scripts/create_cluster.sh
@@ -3,10 +3,10 @@
 CLUSTER_NAME=capstone_cluster
 REGION=us-east-1
 
-if eksctl utils describe-stacks --region=${REGION} --cluster=${CLUSTER_NAME} | grep -q ${CLUSTER_NAME}; then
+if /.eksctl utils describe-stacks --region=${REGION} --cluster=${CLUSTER_NAME} | grep -q ${CLUSTER_NAME}; then
     echo "Cluster already exists!"
 else
     echo "Creating Cluster..."
-    eksctl create cluster --name ${CLUSTER_NAME} --region=${REGION}
+    /.eksctl create cluster --name ${CLUSTER_NAME} --region=${REGION}
     echo "Cluster Created."
 fi
