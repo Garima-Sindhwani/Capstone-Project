d52f29dfcd5ce0b145b4162fca07cb6a604a5a6c
Garima Sindhwani <gsindhwani23@gmail.com>
2023-05-08 12:56:05 +0530

    kubectl fix

--- shell_scripts/deploy_app_blue.sh
+++ shell_scripts/deploy_app_blue.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Get Nodes details
-kubectl get nodes
+/.kubectl get nodes
 
 # Create the deployment and loadbalancer
-kubectl apply -f manifests/deploy_blue.yml
+# kubectl apply -f manifests/deploy_blue.yml
 
-# Get details for loadbalancer
-kubectl get service/loadbalancer-service
-# Get details for deployment
-kubectl get deploy,rs,svc,pods
+# # Get details for loadbalancer
+# kubectl get service/loadbalancer-service
+# # Get details for deployment
+# kubectl get deploy,rs,svc,pods
